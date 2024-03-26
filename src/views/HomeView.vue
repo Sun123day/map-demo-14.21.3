7eebb319e94af316d5d2ea45cb13139962f9929b
Sun123day <2478496130@qq.com>
2024-03-26 10:04:19 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>测试一下</div>
+    <el-button @click="handleClick">点我一下</el-button>
+    <div>
+      <MapDemo></MapDemo>
+    </div>
+  </div>
+</template>
+
+<script>
+import MapDemo from '@/views/mapDemo.vue'
+export default {
+  components: {
+    MapDemo,
+  },
+  methods: {
+    handleClick(){
+      console.log("测试一下捏");
+    },
+  }, 
+
+}
+</script>
+
+<style>
+
+</style>
